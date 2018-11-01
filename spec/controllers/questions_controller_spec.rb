518078c117f0bd554dcf4237329c15aeffe95b73
aleksandrcuprov <microposts@gmail.com>
2018-11-01 15:54:31 +0300

    GET #new for questions_controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -14,4 +14,32 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template :index
     end
   end
+
+  describe 'GET #new' do
+    it 'assigns a new Question to @question' do
+      question = create(:question)
+      get :new
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'render new view' do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+  # describe 'POST #create' do
+  #   context 'with valid data' do
+  #     it 'присвоить переменной вновь созданный вопрос' do
+  #       question = create(:question)
+  #       post :create, params: { title: '123', body: '321' }
+  #       expect(assigns(question)).to be_a_new(question)
+  #     end
+  #
+  #     it 'redirect show view' do
+  #       question = create(:question)
+  #       post :create, params: { title: '123', body: '321' }
+  #       expect(response).to redirect_to question
+  #     end
+  #   end
+  # end
 end
