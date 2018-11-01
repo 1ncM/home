dbddd3ce885561334b63c64c191da1c2a2fc61d0
aleksandrcuprov <microposts@gmail.com>
2018-11-01 21:30:01 +0300

    POST #create for questions_controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -16,8 +16,9 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #new' do
+    let(:question) { create(:question) }
+
     it 'assigns a new Question to @question' do
-      question = create(:question)
       get :new
       expect(assigns(:question)).to be_a_new(Question)
     end
@@ -27,19 +28,28 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template :new
     end
   end
-  # describe 'POST #create' do
-  #   context 'with valid data' do
-  #     it 'присвоить переменной вновь созданный вопрос' do
-  #       question = create(:question)
-  #       post :create, params: { title: '123', body: '321' }
-  #       expect(assigns(question)).to be_a_new(question)
-  #     end
-  #
-  #     it 'redirect show view' do
-  #       question = create(:question)
-  #       post :create, params: { title: '123', body: '321' }
-  #       expect(response).to redirect_to question
-  #     end
-  #   end
-  # end
+
+  describe 'POST #create' do
+    context 'with valid data' do
+      it 'saves the new question in the database' do
+        expect { post :create, params: { question: attributes_for(:question)} }.to change(Question, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        post :create, params: { question: attributes_for(:question) }
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+
+    context 'with invalid data' do
+      it 'does not save the question' do
+        expect { post :create, params: { question: attributes_for(:invalid_question) }}.to_not change(Question, :count)
+      end
+
+      it 'render new view' do
+        post :create, params: { question: attributes_for(:invalid_question) }
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
