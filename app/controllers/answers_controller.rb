e8a7b8434e7493489303761d26db040b1ec24dcc
aleksandrcuprov <microposts@gmail.com>
2018-11-26 08:53:30 +0300

    spec: Users can create answer for question

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,24 @@
 class AnswersController < ApplicationController
+  def new
+  end
+  def create
+    load_question
+    @answer = @question.answers.new(answer_params)
+
+    if @answer.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def load_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body, :question_id)
+  end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  describe 'POST #create' do
+    let(:question) {create(:question)}
+    context 'with valid attributes' do
+      it 'save the new answer in the database' do
+        expect {post :create, params: { answer: attributes_for(:answer), question_id: question}}.to change(question.answers,:count).by(1)
+      end
+      it 'redirect to question' do
+        post :create, params: { answer: attributes_for(:answer), question_id: question}
+        expect(response).to redirect_to(assigns(:answer).question)
+      end
+    end
 
+    context 'with invalid attributes' do
+      it 'do not save in database' do
+        expect {post :create, params: {answer: attributes_for(:invalid_answer), question_id: question}}.to_not change(question.answers, :count)
+      end
+      it 'render new view' do
+        post :create, params: { answer: attributes_for(:invalid_answer), question_id: question }
+        expect(response).to render_template(:new)
+      end
+    end
+  end
 end
